7269977d390e3362dc21b53bd4ce41fe06fa377c
Jefferson Souza <97715277+bad7k@users.noreply.github.com>
2022-10-01 01:42:24 -0300

    Base do Site

---
+++ js/script-imc.js
@@ -0,0 +1,30 @@
+
+
+let botao = document.getElementById('botao-resultado');
+botao.addEventListener('click', calcularImc);
+
+function calcularImc(){
+    let altura = document.getElementById('altura-digitada');
+    let peso = document.getElementById('peso-digitado');
+
+
+    if(altura.value > 0 && peso.value > 0 ){
+        //IMC Resultado
+        let imc = (peso.value) / (altura.value) **2;
+        let resultadoImc = document.getElementById('res-imc');
+        let resultadoSituacao = document.getElementById('res-sit');
+       resultadoImc.innerHTML = `IMC: ${imc.toFixed(1)}`;
+
+       //Mensagem Resultado
+       let mensagem = 
+       imc < 18.5 ? "Abaixo do Normal"
+       : imc < 25 ? "Peso Normal"
+       : imc < 30 ? "Sobrepeso"
+       : imc < 40 ? "Obesidade"
+       : "Obesidade Grave";
+        resultadoSituacao.innerHTML = mensagem;
+    }
+    else {
+        alert("Por favor, digite um valor válido!");
+    }
+}
